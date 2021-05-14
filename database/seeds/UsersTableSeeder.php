440cc0a887ac307a6daee0377e619308ecddbd8c
rui <1975261197@qq.com>
2021-05-14 14:51:24 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
