fc0f368e658ef8adbcbb6862466eed5901f6cac0
rui <1975261197@qq.com>
2021-05-15 09:51:34 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,40 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at','desc');
     }
+    //获取粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+    //获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注功能
+    public function follow($user_ids)
+    {
+        if( ! is_array($user_ids))
+        {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if(! is_array($user_ids))
+        {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断是否关注  关注列表有没 user_id
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
